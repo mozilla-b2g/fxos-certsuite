f5934f90244544e91b01362e18777dd0802195e2
Malini Das <mdas@mozilla.com>
2014-03-11 16:34:02 -0400

    use 'devicelight' for v1.3

--- webapi_tests/webapi_tests/test_proximity.py
+++ webapi_tests/webapi_tests/test_proximity.py
@@ -3,8 +3,7 @@ from webapi_tests import MinimalTestCase
 class TestProximity(MinimalTestCase):
     def tearDown(self):
         self.marionette.execute_script("""
-        window.removeEventListener('userproximity', window.wrappedJSObject.prox_function);
-        window.removeEventListener('deviceproximity', window.wrappedJSObject.prox_function);
+        window.removeEventListener('devicelight', window.wrappedJSObject.prox_function);
         """)
         MinimalTestCase.tearDown(self)
     def test_proximity_change(self):
@@ -17,13 +16,9 @@ class TestProximity(MinimalTestCase):
                                   console.log("proximity event" + event);
                                   window.wrappedJSObject.proximityStates.push(event.toString());
                                 };
-        window.addEventListener('userproximity', window.wrappedJSObject.prox_function);
-        window.addEventListener('deviceproximity', window.wrappedJSObject.prox_function);
+        window.addEventListener('devicelight', window.wrappedJSObject.prox_function);
         """
         self.marionette.execute_script(script)
         self.instruct("Move your hand in front of the phone and hit OK when the screen darkens")
         proximity_values = self.marionette.execute_script("return window.wrappedJSObject.proximityStates")
-        print proximity_values
-        # TODO: proximity events don't seem to be firing... but if you replace it with 'touchstart'
-        # and touch the screen instead of moving your hand, you'll see that the logic works
         self.assertNotEqual(0, len(proximity_values))
