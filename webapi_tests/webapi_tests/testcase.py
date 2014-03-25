c33895194abc8f0e0545d1abffdf9f2637495d3e
Andreas Tolfsen <ato@mozilla.com>
2014-03-25 17:46:05 +0000

    Reuse certapp setup/teardown code in both harnesses
    The patch also clarifies the prompt, instruct, and confirm methods.

--- webapi_tests/webapi_tests/testcase.py
+++ webapi_tests/webapi_tests/testcase.py
@@ -4,7 +4,9 @@ import time
 import mozdevice
 from marionette import Marionette, MarionetteTestCase, MarionetteException
 
-class MinimalTestCase(MarionetteTestCase):
+from certapp import CertAppMixin
+
+class MinimalTestCase(MarionetteTestCase, CertAppMixin):
     def __init__(self, *args, **kwargs):
         self.cert_test_app = None
         # Use a new marionette object instead of relying on older references
@@ -19,98 +21,49 @@ class MinimalTestCase(MarionetteTestCase):
         self.close_cert_app()
         super(MinimalTestCase, self).tearDown()
 
-    def use_cert_app(self):
-        # app management is done in the system app
-        self.marionette.switch_to_frame()
-        # TODO: replace this with pkg_resources if we know that we'll be installing this as a package
-        self.marionette.import_script(os.path.join(os.path.dirname(__file__), "app_management.js"))
-        script = "GaiaApps.launchWithName('CertTest App');"
-        try:
-            # NOTE: if the app is already launched, this doesn't launch a new app, it will return
-            # a reference to the existing app
-            self.cert_test_app = self.marionette.execute_async_script(script, script_timeout=5000)
-            self.assertTrue(self.cert_test_app, "Could not launch CertTest App")
-            self.marionette.switch_to_frame(self.cert_test_app["frame"])
-        except MarionetteException as e:
-            self.instruct("Could not launch CertTest app automatically. "
-                          "Please launch by hand.")
-            iframes = self.marionette.execute_script(
-                "return document.getElementsByTagName('iframe').length")
-            for i in range(0, iframes):
-                self.marionette.switch_to_frame(i)
-                if ("certtest" in self.marionette.get_url()):
-                    return
-                self.marionette.switch_to_frame()
-            self.fail("Could not switch into CertTest App")
-        except Exception as e:
-            message = "Unexpected exception: %s" % e
-            self.fail(message)
-        tries = 60
-        while tries > 0:
-            if 'blank' not in self.marionette.get_url():
-                break
-            time.sleep(1)
-            tries -= 1
-        if tries == 0:
-            self.fail("CertTest app did not load in time")
-        self.assertTrue("certtest" in self.marionette.get_url())
+    def prompt(self, message, question=None, message_type=None):
+        """Prompt the user for an arbitrary response.  Returns the users's
+        input.
 
-    def manually_close_app(self):
-        instruction = "Could not close CertTest app automatically. " \
-                      "Please close the app manually by holding down the Home button " \
-                      "and pressing the X above the CertTest app card."
-        try:
-            response = raw_input("\n=== INSTRUCTION ===\n%s\nWere you successful at closing the app? [Y/n]\n" % instruction) or 'y'
-            while response not in ['y', 'n']:
-                response = raw_input("Please enter 'y' or 'n': ") or 'y'
-        except KeyboardInterrupt:
-            self.fail("Test interrupted by user")
-        if response == 'n':
-            print "Must reboot"
-            dm = mozdevice.DeviceManagerADB()
-            dm.reboot(wait=True)
-            self.instruct("Please unlock the lockscreen after device reboots")
-            dm.forward("tcp:2828", "tcp:2828")
-            self.marionette = Marionette()
-            self.marionette.start_session()
-            self.fail("Failed attempts at closing app.")
+        This will block until the user responds.
 
-    def close_cert_app(self):
-        # TODO: replace this with pkg_resources if we know that we'll
-        # be installing this as a package
-        self.marionette.import_script(
-            os.path.join(os.path.dirname(__file__), "app_management.js"))
-        # app management is done in the system app
-        self.marionette.switch_to_frame()
-        if self.cert_test_app and "origin" in self.cert_test_app:
-            try:
-                script = "GaiaApps.kill('%s');" % self.cert_test_app["origin"]
-                self.marionette.execute_async_script(script, script_timeout=5000)
-                self.assertTrue('certtest' not in self.marionette.get_url())
-            except MarionetteException as e:
-                self.manually_close_app()
-            except Exception as e:
-                message = "Unexpected exception: %s" % e
-                self.fail(message)
-        else:
-            self.manually_close_app()
+        Sample usage::
 
-    def prompt(self, message_type, message, question):
+             answer = self.prompt("What's the meaning of life?")
+             assert answer == "42"
+
+        :param message: The question to ask or message to give the
+            user.
+
+        :returns: The response the user enters as a string.
+
+        """
+
         response = None
+        if not question:
+            question = "Were you successful?"
+        if not message_type:
+            message_type = "PROMPT"
+
         print("\n\n=== %s ===\n%s" % (message_type, message))
         try:
             while response not in ["y", "n", ""]:
                 response = raw_input("%s [Yn] " % question).lower()
         except (KeyboardInterrupt, EOFError):
             self.fail("Test interrupted by user")
-        if response == "n":
-            self.fail("Failed on step: %s" % message)
+
+        return response
 
     def instruct(self, message):
-        self.prompt("INSTRUCTION", message, "Were you successful?")
+        response = self.prompt(message, message_type="INSTRUCTION")
+        if response == "n":
+            self.fail("Failed on instruction: %s" % message)
 
     def confirm(self, message):
-        self.prompt("CONFIRMATION", message, "Response: ")
+        response = self.prompt(
+            message, question="Response: ", message_type="CONFIRMATION")
+        if response == "n":
+            self.fail("Failed to confirm: %s" % message)
 
     def unplug_and_instruct(self, message):
         self.instruct(
