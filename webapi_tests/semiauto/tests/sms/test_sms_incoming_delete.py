d8ea8cbea7e6b67790096824cb0278d7c6053dfb
rwood-moz <rwood@mozilla.com>
2014-03-27 12:00:44 -0400

    First set of SMS tests

---
+++ webapi_tests/semiauto/tests/sms/test_sms_incoming_delete.py
@@ -0,0 +1,30 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from tests import TestCase, test
+from tests.sms import SmsTestCommon
+
+
+class TestSmsIncomingDelete(TestCase, SmsTestCommon):
+    def tearDown(self):
+        self.marionette.execute_script("""
+            SpecialPowers.removePermission("sms", document);
+            SpecialPowers.setBoolPref("dom.sms.enabled", false);
+        """)
+        TestCase.tearDown(self)
+
+    @test
+    def test_sms_incoming_delete(self):
+        self.user_guided_incoming_sms()
+
+        # delete fails sometimes without a sleep (because of the msg notification?)
+        time.sleep(5)
+
+        # delete the SMS using the webapi
+        sms_to_delete = self.in_sms['id']
+        self.delete_message(sms_to_delete)
+
+        # now verify the message has been deleted by trying to get it, should fail
+        error_message = "mozMobileMessage.getMessage found the deleted SMS but shouldn't have; delete failed"
+        self.get_message(sms_to_delete, False, error_message)
