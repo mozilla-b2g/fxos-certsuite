a8ab973c3d6209b8ca79a7314df3dc4fd2d3a050
Andreas Tolfsen <ato@mozilla.com>
2014-03-28 17:57:16 +0000

    Remove unnecessary imports

--- webapi_tests/semiauto/tests/__init__.py
+++ webapi_tests/semiauto/tests/__init__.py
@@ -1,13 +1,3 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from tests import *
-from test_sms import TestSms
-
-
-def all(handler):
-    env = environment.get(InProcessTestEnvironment)
-    suite = unittest.TestSuite()
-    suite.addTest(TestSms("test_navigate", handler=handler))
-    return suite
