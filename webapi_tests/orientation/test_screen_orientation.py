a9e2e39ce8ac3b774c649edb6cbacaa8c230c6d4
rwood-moz <rwood@mozilla.com>
2014-04-09 16:20:45 -0400

    Convert mdas and ato's tests to the semiauto framework

---
+++ webapi_tests/orientation/test_screen_orientation.py
@@ -0,0 +1,36 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from semiauto import TestCase
+
+
+class TestScreenOrientation(TestCase):
+    def check_orientation(self, mode):
+        orientation = self.marionette.execute_script("return window.wrappedJSObject.screen.mozOrientation;")
+        self.assertTrue(mode in orientation, "the screen.mozOrientation value is incorrect/not what was expected")
+
+    def test_orientation_change(self):
+        self.instruct("Ensure the phone is unlocked and in portrait mode")
+        self.check_orientation("portrait")
+        self.instruct("Now rotate the phone into landscape position and wait for the screen to adjust to landscape mode \
+                      (watch the notifications ribbon at the top).")
+        self.check_orientation("landscape")
+
+    def test_orientation_lock(self):
+        self.instruct("Ensure the phone is unlocked and in portrait mode")
+        self.check_orientation("portrait")
+        self.marionette.execute_script("return window.wrappedJSObject.screen.mozLockOrientation('portrait');")
+        self.instruct("Now rotate the phone into landscape position. The screen should NOT adjust to the new orientation.")
+        self.check_orientation("portrait")
+        self.marionette.execute_script("return window.wrappedJSObject.screen.mozUnlockOrientation();")
+        self.instruct("Now rotate the phone into portrait position, then back to landscape position. " \
+                      "The screen should adjust to the new orientation. You may need to wait a few seconds.")
+        self.check_orientation("landscape")
+        self.marionette.execute_script("return window.wrappedJSObject.screen.mozLockOrientation('landscape');")
+        self.instruct("Now rotate the phone into portrait mode. The screen should NOT adjust to the new orientation.")
+        self.check_orientation("landscape")
+        self.marionette.execute_script("return window.wrappedJSObject.screen.mozUnlockOrientation();")
+        self.instruct("Now rotate the phone into landscape position, then back to portrait position. " \
+                      "The screen should adjust to the new orientation. You may need to wait a few seconds.")
+        self.check_orientation("portrait")
---
+++ webapi_tests/vibration/test_vibrate_basic.py
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import time
+
+from semiauto import TestCase
+
+
+class TestVibrateBasic(TestCase):
+    def test_vibrate_basic(self):
+        self.instruct("Ensure the phone is unlocked, then hold the phone.")
+        self.marionette.execute_script("window.navigator.vibrate(200);")
+        self.confirm("Did you feel a single vibration?")
+        self.instruct("Ensure the phone is unlocked, then hold the phone.")
+        self.marionette.execute_script("window.navigator.vibrate([200]);")
+        self.confirm("Did you feel a single vibration?")
+        self.instruct("Ensure the phone is unlocked, then hold the phone.")
+        self.marionette.execute_script("window.navigator.vibrate([200, 1000, 200]);")
+        time.sleep(1)
+        self.confirm("Did you feel two vibrations, with about 1 second between each pulse?")
