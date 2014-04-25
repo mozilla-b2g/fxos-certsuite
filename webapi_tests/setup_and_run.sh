c855a057c29aa22bd98de93819617f620571a8e3
Jonathan Griffin <jgriffin@mozilla.com>
2014-04-25 14:28:40 -0700

    Update setup_and_run.sh to error out if you don't specify a directory

--- webapi_tests/setup_and_run.sh
+++ webapi_tests/setup_and_run.sh
@@ -4,6 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+if [ "$1" == "" ]; then
+    echo "Must specify a directory"
+    exit
+fi
+
 source setup_venv.sh
 echo "Installing Certified App"
 fxos_appgen --install --type=certified --version=1.3 --all-permissions "CertTest App"
