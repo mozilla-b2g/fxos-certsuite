d939f152c4d9cfefde9e251081291cf400353fd6
James Graham <james@hoppipolla.co.uk>
2014-05-23 10:59:14 +0100

    fixup! Update to the latest version of wptrunner.

--- certsuite/harness.py
+++ certsuite/harness.py
@@ -95,13 +95,18 @@ class DeviceBackup(object):
         logger.info("Saving device state")
         self.backup_path = tempfile.mkdtemp()
 
-        for remote in self.backup_dirs:
-            local = self.local_dir(remote)
-            self.device.getDirectory(remote, local)
+        for remote_path in self.backup_dirs:
+            local_path = self.local_dir(remote_path)
+            if not os.path.exists(local_path):
+                os.makedirs(local_path)
+            self.device.getDirectory(remote_path, local_path)
 
         for remote_path in self.backup_files:
             remote_dir, filename = remote_path.rsplit("/", 1)
-            local_path = os.path.join(self.local_dir(remote_dir), filename)
+            local_dir = self.local_dir(remote_dir)
+            local_path = os.path.join(local_dir, filename)
+            if not os.path.exists(local_dir):
+                os.makedirs(local_dir)
             self.device.getFile(remote_path, local_path)
 
         return self
@@ -112,16 +117,17 @@ class DeviceBackup(object):
     def restore(self):
         logger.info("Restoring device state")
         self.device.remount()
-        for remote in self.backup_dirs:
-            local = self.local_dir(remote)
-            self.device.removeDir(remote)
-            self.device.pushDir(local, remote)
 
         for remote_path in self.backup_files:
             remote_dir, filename = remote_path.rsplit("/", 1)
             local_path = os.path.join(self.local_dir(remote_dir), filename)
             self.device.removeFile(remote_path)
             self.device.pushFile(local_path, remote_path)
+
+        for remote_path in self.backup_dirs:
+            local_path = self.local_dir(remote_path)
+            self.device.removeDir(remote_path)
+            self.device.pushDir(local_path, remote_path)
 
         self.device.reboot(wait=True)
 
