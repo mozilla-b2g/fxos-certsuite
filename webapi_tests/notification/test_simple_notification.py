62c1e3daa1d420f9207958aaa34510c24fb2609c
rwood-moz <rwood@mozilla.com>
2014-06-09 17:14:47 -0400

    Bug 1020627: First semi-auto test for Web Notification API

---
+++ webapi_tests/notification/test_simple_notification.py
@@ -0,0 +1,22 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from semiauto import TestCase
+from notification import NotificationTestCommon
+
+class TestSimpleNotification(TestCase, NotificationTestCommon):
+    def setUp(self):
+        super(TestSimpleNotification, self).setUp()
+        permission = self.get_permission_setting()
+        if permission != "granted":
+            result = self.request_permission()
+            self.assertEqual(result, "granted", "User must grant permission on device for notifications")
+
+    def test_simple_notification(self):
+        text = "Hello from Firefox OS"
+        self.instruct("About to create a new device notification. Please watch the Firefox device's "
+                    "notifcation ribbon at the top of the screen.")
+        self.create_notification(text)
+        self.confirm("A new notification (with the text '%s') should appear on the Firefox OS device's "
+                    "notification ribbon. Did the notification appear and contain the correct text?" % text)
