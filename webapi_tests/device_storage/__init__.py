5b7d1804eca2be0c3a789b7492d52f034245bbe5
Gnaneswar Devuni <gnaneswar.devuni@accenture.com>
2014-06-15 20:42:23 +0000

    Bug 1016295 - Develop first semi-auto test for the device storage API

--- webapi_tests/device_storage/__init__.py
+++ webapi_tests/device_storage/__init__.py
@@ -2,4 +2,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from device_storage_test import DeviceStorageTestCommon
+from webapi_tests.device_storage.device_storage_test import DeviceStorageTestCommon
+from webapi_tests.device_stoage.test_sdcard_storage import TestSdcardStorage
