5ceeec1788a6679b54abf278f6fb072e21834734
Gnaneswar Devuni <gnaneswar.devuni@accenture.com>
2014-07-02 15:13:26 +0530

    Bug 1024404 - Develop first Semi-auto test for Idle API. Implemented ato's review comments

--- webapi_tests/idle/idle_active_test.py
+++ webapi_tests/idle/idle_active_test.py
@@ -10,12 +10,44 @@ class IdleActiveTestCommon(object):
         var lock = navigator.mozSettings.createLock();
         var setting = lock.get('screen.brightness');
         setting.onsuccess = function () {
-            window.wrappedJSObject.brightness = setting.result["screen.brightness"];
-            marionetteScriptFinished(1);
+            var brightness = setting.result["screen.brightness"];
+            marionetteScriptFinished(brightness);
         };
         setting.onerror = function () {
             console.log('An error occured: ' + setting.error);
-            marionetteScriptFinished(0);
+            //returns -1 as brightness can range from 0 to 1 for onsuccess
+            marionetteScriptFinished(-1);
         };
         """)
         return result
+
+    def is_idle_state(self):
+        self.marionette.execute_script("""
+        window.wrappedJSObject.rcvd_idle = false;
+        window.wrappedJSObject.testIdleObserver = {
+            time : 5,
+            onidle : function() {
+                window.navigator.mozPower.screenBrightness = 0.1;
+                window.wrappedJSObject.rcvd_idle = true;
+            }
+        };
+        navigator.addIdleObserver(window.wrappedJSObject.testIdleObserver);
+        """)
+
+    def is_active_state(self):
+        self.marionette.execute_script("""
+        window.wrappedJSObject.rcvd_active = false;
+        window.wrappedJSObject.rcvd_idle = false;
+        window.wrappedJSObject.testActiveObserver = {
+            time : 5,
+            onidle : function() {
+                window.navigator.mozPower.screenBrightness = 0.1;
+                window.wrappedJSObject.rcvd_idle = true;
+            },
+            onactive : function() {
+                window.navigator.mozPower.screenBrightness = 0.5;
+                window.wrappedJSObject.rcvd_active = true;
+            }
+        };
+        navigator.addIdleObserver(window.wrappedJSObject.testActiveObserver);
+        """)
--- webapi_tests/idle/test_idle_active.py
+++ webapi_tests/idle/test_idle_active.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import unittest
 
 from marionette.wait import Wait
 from webapi_tests.semiauto import TestCase
@@ -13,42 +14,44 @@ class TestIdleActive(TestCase, IdleActiveTestCommon):
     def setUp(self):
         self.addCleanup(self.clean_up)
         super(TestIdleActive, self).setUp()
-        self.assertTrue(self.get_screen_brightness(), "Unable to get screen current brightness")
+        #get screen current brightness
+        brightness = self.get_screen_brightness()
+        self.marionette.execute_script("window.wrappedJSObject.brightness = arguments[0];", script_args=[brightness])
+        self.assertNotEqual(brightness, -1, "Unable to get screen current brightness")
         #start test with bright screen
         self.marionette.execute_script("window.navigator.mozPower.screenBrightness = 1.0;")
 
-    def test_idle_active(self):
-        self.instruct("About to test idle. Please click OK and then watch the screen, but do not touch the screen")
-        self.marionette.execute_script("""
-        window.wrappedJSObject.rcvd_active = false;
-        window.wrappedJSObject.rcvd_idle = false;
-        window.wrappedJSObject.testObserver = {
-            time : 5,
-            onactive : function() {
-                window.navigator.mozPower.screenBrightness = 0.5;
-                window.wrappedJSObject.rcvd_active = true;
-            },
-            onidle : function() {
-                window.navigator.mozPower.screenBrightness = 0.1;
-                window.wrappedJSObject.rcvd_idle = true;
-            }
-        };
-        navigator.addIdleObserver(window.wrappedJSObject.testObserver);
-        """)
+    def test_idle_state(self):
+        self.instruct("About to test idle state. Please click OK and then watch the screen, but do not touch the screen")
+
+        #setup idleobserver
+        self.is_idle_state()
 
         #wait for device to go idle
+        wait = Wait(self.marionette, timeout=15, interval=0.5)
+        try:
+            wait.until(lambda m: m.execute_script("return window.wrappedJSObject.rcvd_idle;"))
+        except:
+            self.fail("Failed to attain idle state")
+
+        self.confirm("Did you notice decrease in brightness?")
+
+    @unittest.skip("Bug 1033248 - Device does not enter from idle state to active state")
+    def test_active_state(self):
+        self.instruct("About to test active state. Please click OK and then watch the screen")
+        #setup activeobserver
+        self.is_active_state()
+
         wait = Wait(self.marionette, timeout=10, interval=0.5)
         try:
             wait.until(lambda m: m.execute_script("return window.wrappedJSObject.rcvd_idle;"))
         except:
             self.fail("Failed to attain idle state")
 
-        #confirming decrease in brightness
         self.confirm("Did you notice decrease in brightness?")
+        self.instruct("Touch on the screen to wake up the device")
 
-        self.instruct("Touch the device to wake it up")
-
-        wait = Wait(self.marionette, timeout=30, interval=0.5)
+        wait = Wait(self.marionette, timeout=10, interval=0.5)
         try:
             wait.until(lambda m: m.execute_script("return window.wrappedJSObject.rcvd_active;"))
         except:
@@ -57,6 +60,11 @@ class TestIdleActive(TestCase, IdleActiveTestCommon):
         self.confirm("Did you notice increase in brightness?")
 
     def clean_up(self):
-        self.marionette.execute_script("navigator.removeIdleObserver(window.wrappedJSObject.testObserver);")
+        if self.marionette.execute_script("return window.wrappedJSObject.testIdleObserver;") is not None:
+            self.marionette.execute_script("navigator.removeIdleObserver(window.wrappedJSObject.testIdleObserver);")
+        if self.marionette.execute_script("return window.wrappedJSObject.testActiveObserver;") is not None:
+            self.marionette.execute_script("navigator.removeIdleObserver(window.wrappedJSObject.testActiveObserver);")
         #restore brightness
-        self.marionette.execute_script("window.navigator.mozPower.screenBrightness = window.wrappedJSObject.brightness;")
+        if self.marionette.execute_script("return window.wrappedJSObject.brightness;") != -1:
+            self.marionette.execute_script("window.navigator.mozPower.screenBrightness = window.wrappedJSObject.brightness;")
+            self.marionette.execute_script("window.wrappedJSObject.brightness = -1;")
