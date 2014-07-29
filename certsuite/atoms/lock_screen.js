fd12ac37ba50e346e1ccb362e81a3558d5eb171b
Andreas Tolfsen <ato@mozilla.com>
2014-07-29 16:55:05 +0100

    Bug 1043924: Disable screen lock, display sleep, and switch on screen in super harness

---
+++ certsuite/atoms/lock_screen.js
@@ -0,0 +1,60 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+var GaiaLockScreen = {
+
+  unlock: function() {
+    let lockscreen = window.wrappedJSObject.lockScreen || window.wrappedJSObject.LockScreen;
+    let setlock = window.wrappedJSObject.SettingsListener.getSettingsLock();
+    let obj = {'screen.timeout': 0};
+    setlock.set(obj);
+
+    window.wrappedJSObject.ScreenManager.turnScreenOn();
+
+    waitFor(
+      function() {
+        lockscreen.unlock(true);
+        waitFor(
+          function() {
+            finish(lockscreen.locked);
+          },
+          function() {
+            return !lockscreen.locked;
+          }
+        );
+      },
+      function() {
+        return !!lockscreen;
+      }
+    );
+  },
+
+  lock: function() {
+    let lockscreen = window.wrappedJSObject.lockScreen || window.wrappedJSObject.LockScreen;
+    let setlock = window.wrappedJSObject.SettingsListener.getSettingsLock();
+    let obj = {'screen.timeout': 0};
+    setlock.set(obj);
+
+    window.wrappedJSObject.ScreenManager.turnScreenOn();
+
+    waitFor(
+      function() {
+        lockscreen.lock(true);
+        waitFor(
+          function() {
+            finish(!lockscreen.locked);
+          },
+          function() {
+            return lockscreen.locked;
+          }
+        );
+      },
+      function() {
+        return !!lockscreen;
+      }
+    );
+  }
+};
---
+++ certsuite/gaiautils.py
@@ -0,0 +1,63 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+import os
+
+
+class Settings(object):
+    atom = os.path.abspath(
+        os.path.join(__file__, os.path.pardir, "atoms", "data_layer.js"))
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.import_script(Settings.atom)
+
+    def get(self, key):
+        return self.driver.execute_async_script(
+            "return GaiaDataLayer.getSetting('%s')" % key, special_powers=True)
+
+    def set(self, key, value):
+        import json
+        value = json.dumps(value)
+        result = self.driver.execute_script("return GaiaDataLayer.setSetting('%s', %s)" %
+                                            (key, value), special_powers=True)
+
+
+class LockScreen(object):
+    atom = os.path.abspath(
+        os.path.join(__file__, os.path.pardir, "atoms", "lock_screen.js"))
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.import_script(LockScreen.atom)
+
+    @property
+    def is_locked(self):
+        return self.driver.execute_script(
+            "return window.wrappedJSObject.lockScreen.locked")
+
+    def lock(self):
+        self.driver.switch_to_frame()
+        result = self.driver.execute_async_script("GaiaLockScreen.lock()")
+        assert result, "Unable to lock screen"
+
+    def unlock(self):
+        self.driver.switch_to_frame()
+        result = self.driver.execute_async_script(
+            "GaiaLockScreen.unlock()")
+        assert result, "Unable to unlock screen"
+
+
+class Screen(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def on(self):
+        self.driver.execute_script(
+            "window.wrappedJSObject.ScreenManager.turnScreenOn()")
+
+    def off(self):
+        self.driver.execute_script(
+            "window.wrappedJSObject.ScreenManager.turnScreenOff()")
