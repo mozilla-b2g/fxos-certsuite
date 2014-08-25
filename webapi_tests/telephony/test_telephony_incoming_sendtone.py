04041a085396a0f5e9c926695c074fb7d4473215
Gnaneswar Devuni <gnaneswar.devuni@accenture.com>
2014-08-25 14:54:21 +0530

    Bug 1032689 - Develop more semi-auto tests for the Telephony WebAPI. Implemented incoming_sendtone and outgoing_incoming tests for telephony

---
+++ webapi_tests/telephony/test_telephony_incoming_sendtone.py
@@ -0,0 +1,70 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import time
+
+from webapi_tests.semiauto import TestCase
+from webapi_tests.telephony.telephony_test import TelephonyTestCommon
+
+
+class TestTelephonyIncomingSendtone(TestCase, TelephonyTestCommon):
+    """
+    This is a test for the `WebTelephony API`_ which will:
+
+    - Disable the default gaia dialer, so that the test app can handle calls
+    - Setup a mozTelephony event listener for incoming calls
+    - Ask the test user to phone the Firefox OS device from a second phone
+    - Verify that the mozTelephony incoming call event is triggered
+    - Answer the incoming call via the API, keep the call active for 3 seconds
+    - Send a DTMF tone for one character ('0' in this test case) and ask the test user to verify
+    - Stop transmitting the currently playing DTMF tone and ask the test user to verify
+    - Hangup the call via the API
+    - Verify that the corresponding mozTelephonyCall events were triggered
+    - Re-enable the default gaia dialer
+
+    .. _`WebTelephony API`: https://developer.mozilla.org/en-US/docs/Web/Guide/API/Telephony
+    """
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+        TelephonyTestCommon.__init__(self)
+
+    def setUp(self):
+        self.addCleanup(self.clean_up)
+        super(TestTelephonyIncomingSendtone, self).setUp()
+        self.wait_for_obj("window.navigator.mozTelephony")
+        # disable the default dialer manager so it doesn't grab our calls
+        self.disable_dialer()
+
+    def test_telephony_incoming_sendtone(self):
+        # ask user to call the device; answer and verify via webapi
+        self.user_guided_incoming_call()
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['0'], self.incoming_call)
+
+        # answer the incoming call
+        self.answer_call()
+        self.assertEqual(self.active_call_list[0]['state'], "connected", "Call state should be 'connected'")
+        self.assertEqual(self.active_call_list[0]['number'], self.incoming_call['number'])
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['length'], 1, "There should be 1 active call")
+
+        # keep call active for a while
+        time.sleep(3)
+
+        # send a DTMF tone for character '0'
+        self.marionette.execute_script("window.navigator.mozTelephony.startTone(0);")
+        time.sleep(2)
+        self.confirm("Did you hear the DTMF tone?")
+        self.marionette.execute_script("window.navigator.mozTelephony.stopTone();")
+
+        # disconnect the active call
+        self.hangup_call()
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['length'], 0, "There should be 0 calls")
+
+    def clean_up(self):
+        # re-enable the default dialer manager
+        self.enable_dialer()
+        self.active_call_list = []
---
+++ webapi_tests/telephony/test_telephony_outgoing_incoming.py
@@ -0,0 +1,119 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import time
+from marionette.wait import Wait
+
+from webapi_tests.semiauto import TestCase
+from webapi_tests.telephony.telephony_test import TelephonyTestCommon
+
+
+class TestTelephonyOutgoingIncoming(TestCase, TelephonyTestCommon):
+    """
+    This is a test for the `WebTelephony API`_ which will:
+
+    - Disable the default gaia dialer, so that the test app can handle calls
+    - Ask the test user to specify a destination phone number for the test call
+    - Setup mozTelephonyCall event listeners for the outgoing call
+    - Use the API to initiate the outgoing call
+    - Ask the test user to answer the call on the destination phone
+    - Keep the call active for 5 seconds
+    - While first call is active, ask the test user to phone the Firefox OS device from third phone
+    - Verify that the mozTelephony incoming call event is triggered
+    - Answer the incoming call via the API
+    - Verify that the first call state should be on held while second call becomes active
+    - Hang up the connected call via the API
+    - Verify the held call is now resumed and the only active call
+    - Hang up the remaining active call via the API
+    - Verify that the corresponding mozTelephonyCall events were triggered
+    - Re-enable the default gaia dialer
+
+    .. _`WebTelephony API`: https://developer.mozilla.org/en-US/docs/Web/Guide/API/Telephony
+    """
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+        TelephonyTestCommon.__init__(self)
+
+    def setUp(self):
+        self.addCleanup(self.clean_up)
+        super(TestTelephonyOutgoingIncoming, self).setUp()
+        self.wait_for_obj("window.navigator.mozTelephony")
+        # disable the default dialer manager so it doesn't grab our calls
+        self.disable_dialer()
+
+    def test_telephony_outgoing_incoming(self):
+        # use the webapi to make an outgoing call to user-specified number
+        self.user_guided_outgoing_call()
+        # verify one outgoing call
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['length'], 1, "There should be 1 call")
+        self.assertEqual(self.calls['0'], self.outgoing_call)
+
+        # have user answer the call on target
+        self.answer_call(incoming=False)
+
+        # keep call active for a while
+        time.sleep(5)
+
+        # verify the active call
+        self.assertEqual(self.active_call_list[0]['number'], self.outgoing_call['number'])
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['length'], 1, "There should be 1 active call")
+        self.assertEqual(self.active_call_list[0]['state'], "connected", "Call state should be 'connected'")
+
+        # ask user to call the device; answer and verify via webapi
+        self.user_guided_incoming_call()
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['1'], self.incoming_call)
+        self.assertEqual(self.calls['0'], self.active_call_list[0])
+
+        # setup the 'onheld' event handler
+        self.hold_active_call(user_initiate_hold=False)
+
+        # answer the incoming call
+        self.answer_call()
+        time.sleep(5)
+        self.assertEqual(self.active_call_list[1]['state'], "connected", "Call state should be 'connected'")
+        self.assertEqual(self.active_call_list[1]['number'], self.incoming_call['number'])
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['length'], 2, "There should be 2 active calls")
+
+        wait = Wait(self.marionette, timeout=90, interval=0.5)
+        try:
+            wait.until(lambda x: x.execute_script("return window.wrappedJSObject.onheld_call_ok"))
+        except:
+            # failed to hold
+            self.fail("Failed to put first active call on hold while second call becomes active")
+
+        onholding = self.marionette.execute_script("return window.wrappedJSObject.onholding_call_ok")
+        self.assertFalse(onholding, "Telephony.onholding event found, but should not have been "
+                            "since the phone user did not initiate holding the call")
+
+        # keep call active for a while
+        time.sleep(5)
+
+        # verify call state change
+        self.assertEqual(self.calls['0']['state'], "held", "Call state should be 'held'")
+        self.assertEqual(self.calls['1']['state'], "connected", "Call state should be 'connected'")
+
+        # disconnect the two active calls
+        self.hangup_call(active_call_selected=1)
+
+        # keep a delay to get the change in call state
+        time.sleep(2)
+
+        # verify number of remaining calls and its state
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['length'], 1, "There should be 1 active call")
+        self.assertEqual(self.calls['0']['state'], "connected", "Call state should be 'connected'")
+
+        self.hangup_call(active_call_selected=0)
+        self.calls = self.marionette.execute_script("return window.wrappedJSObject.calls")
+        self.assertEqual(self.calls['length'], 0, "There should be 0 calls")
+
+    def clean_up(self):
+        # re-enable the default dialer manager
+        self.enable_dialer()
+        self.active_call_list = []
