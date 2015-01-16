19f2bd9833b5dc33ca443c327b609737153dbc5e
OooO <otalimu@gmail.com>
2015-01-16 22:54:18 +0800

    remove unused webapi test for stingray

--- webapi_tests/fm_radio/__init__.py
+++
@@ -1,6 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this file,
-# You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from webapi_tests.fm_radio.fm_radio_test import FMRadioTestCommon
-from webapi_tests.fm_radio.test_fm_radio_basic import TestFMRadioBasic
